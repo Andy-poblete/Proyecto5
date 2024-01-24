d0b924aeb5bb4e17af1f747c6ea242dcbfd83b9a
Andy-poblete <andy.poblete19@gmail.com>
2024-01-23 21:37:54 -0300

    Se realizan pequeñas modificaciones de los productos

--- models/Product.js
+++ models/Product.js
@@ -1,19 +1,47 @@
 const mongoose = require('mongoose');
 
-const categorie = new mongoose.Schema({
-
-})
-
-
 const productSchema = new mongoose.Schema({
-    sku: Number, 
-    name: String,
-    price: String,
-    precio: Number,
+    sku: {
+        type: Number,
+        required: true,
+        minLenght: 6,
+        maxLenght: 6
+    },
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+        minLenght: 3,
+        maxLenght: 130,
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 10000000
+    },
     image: String,
-    descripcion: String,
-    size: String,
-    stock: String
+    description: {
+        type: String,
+        required: true,
+        lowercase: true,
+        minLenght: 3,
+        maxLenght: 800,
+        trim: true,
+    },
+    size: {
+        type: String,
+        required: true,
+        minLenght: 1,
+        maxLenght: 6,
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 1000
+    },
 })
 
 const Product = mongoose.model("productos", productSchema)
